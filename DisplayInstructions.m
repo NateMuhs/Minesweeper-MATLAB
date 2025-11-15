fe9909b9f7fc74a1baac59d24fa1adb42d4d3c7e
Nathaniel Muhs <152720079+NateMuhs@users.noreply.github.com>
2025-11-15 18:43:44 -0500

    Added the project files
    I added the project files with the functions that initially created

---
+++ DisplayInstructions.m
@@ -0,0 +1,21 @@
+function DisplayInstructions()
+%DISPLAYINSTRUCTIONS displays the instructions for the minesweeper game
+%
+%   Input: None
+%
+%   Output: None
+
+disp('You are provided with a 5x5 game board for the Minesweeper game.');
+disp('Within this 5x5 game board there are 3 randomly placed mines');
+disp('for you to diffuse. At the start of each turn, the current game');
+disp('board will be shown along with a prompt to either diffuse a bomb');
+disp('or investigate a location. There are three things that can happen');
+disp('when investigating a space:');
+disp('  - If the location has a bomb, it will explode and the game will end.');
+disp('  - If there are zero mines around it, all connected zero-mine spaces');
+disp('    will be revealed.');
+disp('  - Otherwise, the investigated space will display a nonzero number');
+disp('    of mines located around it.');
+disp('The game ends when all mines are successfully diffused.');
+
+end
