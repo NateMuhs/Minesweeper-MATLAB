e64b1185eb89beac7883676d527f9a317905f12c
natem <natem@NatesLaptop>
2025-11-16 15:08:53 -0500

    added file

---
+++ MineSweeper-Project-Files/game_loop.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = game_loop(inputArg1,inputArg2)
+%GAME_LOOP Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
